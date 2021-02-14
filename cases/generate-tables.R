14fe4865f3cede325a895b910c2b5dd7d01299d3
wwiecek <20645798+wwiecek@users.noreply.github.com>
2021-02-14 21:34:19 +0000

    tables gen

---
+++ cases/generate-tables.R
@@ -0,0 +1,46 @@
+library(xtable)
+
+table_g3.kappa <- df_kappa %>%
+  mutate(model = factor(model, levels = c("pars_le_cr", "pars_le_slow", "pars_le_fast"),
+                        labels = c("Constant risk", "Slow growth", "Fast growth"))) %>%
+  filter(d1 %in% c(d1_general, Inf)) %>%
+  mutate(ref_i = i[d1 > 1460]) %>%
+  mutate(ref_d = d[d1 > 1460]) %>%
+  ungroup() %>%
+  mutate(ri = 1-(i/ref_i)) %>%
+  mutate(rd = 1-(d/ref_d)) %>%
+  group_by(d1, model, e, kappa) %>%
+  filter(e == .95) %>%
+  mutate(d1 = as.percent(1/d1)) %>%
+  select(d1, model, i, ri, d, rd) %>%
+  mutate(d = round(d*1e05)) %>%
+  gather(variable, value, -d1, -model, -e, -kappa) %>%
+  mutate(variable = factor(variable, 
+                           levels = c("i", "d", "ri", "rd"),
+                           labels = c("Infections", "Deaths per 100,000", "RI", "RD"))) %>%
+  mutate(value = round(value, 2)) %>%
+  spread(d1, value) %>% 
+  arrange(variable, model) %>% ungroup() %>%
+  select(-e)
+
+
+table_g3 <- df_efficacy_delta %>% 
+  filter(e == .95) %>%
+  filter(d1 %in% c(d1_general, Inf)) %>% 
+  mutate(d1 = as.percent(1/d1)) %>%
+  select(d1, model, i, ri, d, rd) %>%
+  mutate(d = round(d*1e05)) %>%
+  # mutate(i = i*1e05) %>%
+  gather(variable, value, -d1, -model, -e) %>%
+  mutate(variable = factor(variable, 
+                           levels = c("i", "d", "ri", "rd"),
+                           labels = c("Infections", "Deaths per 100,000", "RI", "RD"))) %>%
+  mutate(value = round(value, 2)) %>%
+  spread(d1, value) %>% 
+  arrange(variable, model) %>% ungroup() %>%
+  select(-e)
+
+
+print ("Generating tables G3 and the analagous for kappa")
+print (xtable(table_g3))
+print (xtable(table_g3.kappa,digits=c(0,5,0,0,2,2,2,2,2,2)))
