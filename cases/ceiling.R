6b80e7690fa5f8a5f0daeaf9c2dac45d3bed24bd
asimoesgomes <asimoes@stanford.edu>
2021-02-25 02:04:48 -0600

    Modified script and generated Rdata file

--- cases/ceiling.R
+++ cases/ceiling.R
@@ -1,13 +1,47 @@
 library(kableExtra)
+library(tidyverse)
+load("data/default_inputs.Rdata")
+
+fig_folder <- "figures"
+
+# Main FDF assumptions
+delay_default <- 28 - 10
+delay_fdf <- 84 - 10
+delay_hybrid <- c(rep(delay_fdf, 6), rep(delay_default, 3))
+
+# Demographics (for comparing HIC vs LIC)
+hic_pop <- pbc_spread[countries["High-income countries"],] %>% as.numeric()
+lic_pop <- pbc_spread[countries["Low-income countries"],] %>% as.numeric()
+ifr_hic <- c(0.002, 0.006, 0.03, 0.08, 0.15, 0.60, 2.2, 5.1, 9.3)/100
+ifr_lic <- ifr_hic*(3.2/2)^(5:(-3))
+
+default_pdeath <- ifr_hic
+# use_delta <- TRUE
+
+# Case with losing immunity
+kappa_default <- 0
+
+def_labels <- list(
+  "speed" = "Fraction vaccinated each day, delta"
+)
+
+# default_speeds <- c(seq(60, 360, 10), 450, 540, 630, 730, 1460, Inf)
+# main3speeds <- c(360, 180, 90)
+default_speeds <- round(100/c(2, rev(seq(.05, 1, .05)), .025, .01, 0), 5) # % per day
+fdf_speeds <- rev(round(100/c(.1, .25, .5, .75, 1, 2), 5))
+# d1_general <- c(90, 120, 180, 360, 730, 1460)
+d1_general <- 100/c(2, 1, .75, .5, .25, .1) # % per day
+default_delta_value <- .0025 #for LE scenario
+# le_speeds <- round(100/c(.25, .3, .4, .5, .75, 1), 5)
+le_speeds <- round(100/c(.25, .3, .35, .4, .5, 1, 2), 5)
 
 country_case <- list(list('hic',1),list('hic',0.25),list('lic',1),list('lic',0.25))
 
 gg1.out.ceiling <- data.frame()
 gg2.out.ceiling <- data.frame()
-df_efficacy_delta.out.ceiling <- data.frame()
+df_efficacy_delta_raw.out.ceiling <- data.frame()
 df_fdf.out.ceiling <- data.frame()
 
-####Running all cases####
 for (d in country_case){
   country <- as.character(d[1])
   default_supply_ceiling <- as.numeric(d[2])
@@ -17,72 +51,35 @@ for (d in country_case){
   if (country=='lic'){
     pop <- lic_pop/sum(lic_pop)
   }
-  #I thought redefining defaul_supply_ceiling would be enough but I get different results when calling prioritisation at every iteration
-  source("R/prioritisation.R")
   
-  ##Dataframe for table g3 and the plot for harm reduction (can also be used for the lower efficacy analysis)
-  df_efficacy_delta_raw.ceiling <- expand_grid(d1 = default_speeds,
-                                               e = seq(.5, .95, .05),
-                                               model = c("pars_le_cr", "pars_le_slow", "pars_le_fast")) %>%
-    mutate(data = pmap(list(model, d1, e), function(x,y, z) data.frame(value = model_i(x,y, z), 
-                                                                       var = metric_nms))) %>%
-    unnest(data) %>%
-    spread(var, value) %>%
-    group_by(model, e)  %>%
-    mutate(model = factor(model, levels = c("pars_le_cr", "pars_le_slow", "pars_le_fast"),
-                          labels = c("Constant risk", "Slow growth", "Fast growth"))) 
+  source("setup.R")
   
-  df_efficacy_delta.ceiling <- df_efficacy_delta_raw.ceiling %>%
-    filter(round(d1,4) %in% c(round(d1_general,4), Inf)) %>%
-    mutate(ref_e = harm[d1 > 1460]) %>%
-    mutate(ref_i = i[d1 > 1460]) %>%
-    mutate(ref_d = d[d1 > 1460]) %>%
-    ungroup() %>%
-    mutate(re = 1-(harm/ref_e)) %>%
-    mutate(ri = 1-(i/ref_i)) %>%
-    mutate(diffi = (i-ref_i)) %>%
-    mutate(rd = 1-(d/ref_d)) %>%
-    mutate(diffd = (d-ref_d)) %>%
-    group_by(d1, model, e)
+  source("cases/prep-delta-for-fdf.R")
+  source("cases/first-doses-first-v2.R")
   
-  df_efficacy_delta.ceiling$ceiling <- default_supply_ceiling
-  df_efficacy_delta.ceiling$country <- country
+  source("cases/prep-results.R")
+  source("cases/general-example.R")
+  source("cases/lower-efficacy.R")
+  source("cases/lower-efficacy-delay.R")
   
-  df_efficacy_delta.out.ceiling <- rbind(df_efficacy_delta.out.ceiling,df_efficacy_delta.ceiling)
+  source("cases/kappa-impact.R")
+  
+  df_efficacy_delta_raw.ceiling <- df_efficacy_delta_raw
+  
+  df_efficacy_delta_raw.ceiling$ceiling <- default_supply_ceiling
+  df_efficacy_delta_raw.ceiling$country <- country
+  
+  df_efficacy_delta_raw.out.ceiling <- rbind(df_efficacy_delta_raw.out.ceiling,df_efficacy_delta_raw.ceiling)
   
   ##Dataframes for figure g1 (evolution of infection/death/vaccination over time)
-  gg1.ceiling <- lapply(mlist, function(pars) {
-    ll <- list(
-      "Vaccinate 0.3% per day" = apap_2v(pars, 100/.3),
-      "Vaccinate 0.5% per day" = apap_2v(pars, 100/.5),
-      "Vaccinate 1% per day"  = apap_2v(pars, 100/1),
-      "No vaccination" = list_modify(pars, delta1 = rep(0, Ngroups))) %>%
-      lapply(sr, f = "2v_v2") %>%
-      lapply(rescale_rcs, pop, merge=T) %>% 
-      abind::abind()
-    as.data.frame(ll[,"I",]) %>%
-      mutate(time = 1:nrow(.))
-  }) %>%
-    bind_rows(.id = "scenario") %>%
-    gather(var, value, -time, -scenario)
+  gg1.ceiling <- gg1.df
   
   gg1.ceiling$ceiling <- default_supply_ceiling
   gg1.ceiling$country <- country
   
   gg1.out.ceiling <- rbind(gg1.out.ceiling,gg1.ceiling)
   
-  pars <- mlist[[1]]
-  ll <- list(
-    "Vaccinate 0.3% per day" = apap_2v(pars, 100/.3),
-    "Vaccinate 0.5% per day" = apap_2v(pars, 100/.5),
-    "Vaccinate 1% per day"  = apap_2v(pars, 100/1),
-    "No vaccination" = list_modify(pars, delta1 = rep(0, Ngroups))) %>%
-    lapply(sr, f = "2v_v2") %>%
-    lapply(rescale_rcs, pop, merge=T) %>% 
-    abind::abind()
-  gg2.ceiling <- as.data.frame(ll[,"cumV",]) %>%
-    mutate(time = 1:nrow(.)) %>%
-    gather(var, value, -time)
+  gg2.ceiling <- gg2.df
   
   gg2.ceiling$ceiling <- default_supply_ceiling
   gg2.ceiling$country <- country
@@ -90,23 +87,30 @@ for (d in country_case){
   gg2.out.ceiling <- rbind(gg2.out.ceiling,gg2.ceiling)
   
   ##Dataframes for the fdf analysis
-  df_fdf.ceiling <- expand.grid(d1 = c(fdf_speeds, Inf),
-                                model = c("pars_le_cr", "pars_le_slow", "pars_le_fast"), 
-                                e = c(.4, .5, .6, .7, .8, .9, .95), 
-                                policy = c("default", "fdf", "hybrid")) %>%
-    mutate(data = pmap(list(model, d1, e, policy), 
-                       function(x,y,z,a) data.frame(value = model_fdf(x,y,z,a), 
-                                                    var = metric_nms))) %>%
-    unnest(data) %>%
-    spread(var, value) %>%
-    mutate(model = factor(model, levels = c("pars_le_cr", "pars_le_slow", "pars_le_fast"),
-                          labels = c("Constant risk", "Slow growth", "Fast growth"))) %>%
-    group_by(d1, model, e, policy)
+  df_fdf.ceiling <- df_fdf
   
   df_fdf.ceiling$ceiling <- default_supply_ceiling
   df_fdf.ceiling$country <- country
   df_fdf.out.ceiling <- rbind(df_fdf.out.ceiling,df_fdf.ceiling)
 }
+
+df_efficacy_delta.out.ceiling <- 
+  df_efficacy_delta_raw.out.ceiling %>%
+  ungroup() %>%
+  # filter(d1 %in% c(90, 180, 360, 730, 1460, Inf)) %>%
+  filter(d1 %in% c(d1_general, Inf)|d1==133.33333) %>%
+  group_by(model, e,ceiling,country) %>% 
+  # mutate(benefit_vr = 1 - harm_vr) %>%
+  mutate(ref_e = harm[d1 > 1460]) %>%
+  mutate(ref_i = i[d1 > 1460]) %>%
+  mutate(ref_d = d[d1 > 1460]) %>%
+  ungroup() %>% 
+  group_by(d1, model, e,ceiling,country) %>% 
+  mutate(re = 1-(harm/ref_e)) %>%
+  mutate(ri = 1-(i/ref_i)) %>%
+  mutate(diffi = (i-ref_i)) %>%
+  mutate(rd = 1-(d/ref_d)) %>%
+  mutate(diffd = (d-ref_d))
 
 ####Preparing merged dataframes####
 
@@ -170,6 +174,8 @@ g2.ceiling.fig <- ggarrange(g2b.out.ceiling %>% filter(delta1!=0&country=="HIC")
                               ylab("Fraction of harm averted") + ggtitle("LIC"),
                             common.legend = TRUE, ncol = 1)
 
+ggsave(paste0(fig_folder, "/ceiling.pdf"), g2.ceiling.fig, width = 5.55, height=5.55)
+
 ####Final number of infections and death cases (table - G3 equivalent)####
 g3.out.ceiling <- g3.out.ceiling %>% 
   mutate(country = factor(country,
@@ -177,7 +183,7 @@ g3.out.ceiling <- g3.out.ceiling %>%
                           labels = c("HIC", "LIC"))) %>% 
   rename(region=country)
 
-table.ceiling <- g3.out.ceiling %>% filter(variable %in% c("Infections","Deaths per 100")) %>% 
+table.ceiling <- g3.out.ceiling %>% filter(variable %in% c("Infections","Deaths per 100,000")) %>% 
   arrange(region,desc(ceiling),variable,model) %>% 
   select(variable,model,`0%`,`0.25%`,`0.5%`,`0.75%`,`1%`,`2%`)
 # write.csv(table.ceiling,'results/ceiling.csv')
@@ -231,5 +237,7 @@ g1_joint.ceiling <- ggarrange(gg2.plot.ceiling + ggtitle("Vaccinations")+ theme(
                               gg1.plot.ceiling + ggtitle("Infections")+ theme(text = element_text(size=7)), 
                               widths = c(1,2.5))
 
-ggsave(paste0(fig_folder, "/ceiling.pdf"), g2.ceiling.fig, width = 5.55, height=5.55)
 ggsave(paste0(fig_folder, "/g1_joint_ceiling.pdf"),g1_joint.ceiling, width = 5.55, height=2)
+
+save(df_efficacy_delta_raw.out.ceiling,df_fdf.out.ceiling,gg1.out.ceiling,gg2.out.ceiling,
+     file = "results/results_ceiling_analysis.Rdata")
