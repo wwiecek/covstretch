69781cbaeea850a398cdae8b060fbb27f735e127
asimoesgomes <55849180+asimoesgomes@users.noreply.github.com>
2021-03-30 05:15:31 -0500

    New nlopt results and adjustments (#15)
    * Changes of dimensions in plots and adding supply ceiling analysis
    
    * Modified script and generated Rdata file
    
    * Fix figure g2, add vax_rates, and readme
    
    * fix references
    
    * Vax rates table
    
    * Separating vax rates
    
    * Color intensity figure fdf2
    
    * Edits figures, fix supply_impact
    
    * Adding group sequence to prioritisation
    
    * Compatible for group varying vax rates
    
    * Fixing another numerical issue
    
    * Sequential vax for all groups and hybrid fdf
    
    * Improving generalized hybrid
    
    * Updated figure allk
    
    * Fixing generalized hybrid fdf script
    
    * New fdf-delta files
    
    * Case for expand from smaller than delay
    
    * Changes of dimensions in plots and adding supply ceiling analysis
    
    * Separating vax rates
    
    * Compatible for group varying vax rates
    
    * Improving generalized hybrid
    
    * Fixing generalized hybrid fdf script
    
    * General hybrid strategies fix
    
    * nlopt adjustments
    
    * opt evaluation 7-groups
    
    * Tests nlopt

--- optimisation-epi/compare-bfs-nloptr.R
+++ optimisation-epi/compare-bfs-nloptr.R
@@ -1,4 +1,4 @@
-# Compare the results with brute force approach -----
+# Compare the nlopt with brute force approach - 2 groups -----
 load("results/explore-2groups.Rdata")
 load("results/wip-nl-solutions.Rdata")
 
@@ -29,4 +29,77 @@ rbind(
   mutate(mixing = factor(homogeneous, levels = c(0,1), labels = c("Heterogen.", "Homogen."))) %>%
   ggplot(aes(x = age3, y = age9, color = opt, pch = factor(d1))) + 
   geom_point(size = 2.5) + facet_grid(mixing ~ .)
+
+# Comparison of nlopt and bfs - 7 groups ----
+load("results/opt-bfs-result7.Rdata")
+#Change the file below, the 'variable' filter, and the outcome in model_fd_static for deaths/infections
+load("results/wip-nl-solutions7-I.Rdata")
+
+#Static case
+
+unroll_x <- function(x,sub=1) c(sub,sub,x[1],x[2],x[3],x[4],x[5],x[6],x[7])
+
+eval_g_ineq <- function(x) c(x[1]*pop[3]+x[2]*pop[4]+x[3]*pop[5]+x[4]*pop[6]+x[5]*pop[7]+x[6]*pop[8]+x[7]*pop[9]-Q,
+                             -x[1]*pop[3]-x[2]*pop[4]-x[3]*pop[5]-x[4]*pop[6]-x[5]*pop[7]-x[6]*pop[8]-x[7]*pop[9]+Q)
+
+df_comp <- data.frame()
+for (Q in unique(nl_q_seq)){
+  res_bfs_0 <- as.numeric(res_static[(res_static$q==round(Q,2))&(res_static$homogeneous==0)&(res_static$variable=='i')
+                                   ,c('age3','age4','age5','age6','age7','age8','age9')])
+  res_bfs_1 <- as.numeric(res_static[(res_static$q==round(Q,2))&(res_static$homogeneous==1)&(res_static$variable=='i')
+                                   ,c('age3','age4','age5','age6','age7','age8','age9')])
+  eval_bfs_0 <- model_fd_static(unroll_x(res_bfs_0,sub=0), 
+                                homogen = 0, 
+                                outcome = "cumI",
+                                ret = 1)
+  eval_bfs_1 <- model_fd_static(unroll_x(res_bfs_1,sub=0), 
+                                homogen = 1, 
+                                outcome = "cumI",
+                                ret = 1)
+  eval_ineq_bfs_0 <- eval_g_ineq(res_bfs_0)[1]
+  eval_ineq_bfs_1 <- eval_g_ineq(res_bfs_1)[1]
+  
+  eval_nlopt_0 <- nlopt_s0[1,as.character(Q)]
+  res_nlopt_0 <- nlopt_s0[2:8,as.character(Q)]
+  eval_ineq_nlopt_0 <- eval_g_ineq(res_nlopt_0)[1]
+  eval_nlopt_1 <- nlopt_s1[1,as.character(Q)]
+  res_nlopt_1 <- nlopt_s1[2:8,as.character(Q)]
+  eval_ineq_nlopt_1 <- eval_g_ineq(res_nlopt_1)[1]
+  
+  df_comp <- rbind(df_comp, data.frame("eval_bfs_0"=eval_bfs_0,"eval_nlopt_0"=eval_nlopt_0,"constr_bfs_0"=eval_ineq_bfs_0,
+                                       "constr_nlopt_0"=eval_ineq_nlopt_0,"eval_bfs_1"=eval_bfs_1,"eval_nlopt_1"=eval_nlopt_1,
+                                       "constr_bfs_1"=eval_ineq_bfs_1,"constr_nlopt_1"=eval_ineq_nlopt_1,"q"=Q))
+}
+#Comparing objective function and checking constraints for nlopt - h=0
+print (dim(df_comp %>% filter(eval_nlopt_0<=eval_bfs_0))[1]/dim(df_comp)[1])
+print (dim(df_comp %>% filter(eval_ineq_nlopt_0<=0.0001))[1]/dim(df_comp)[1])
+#Comparing objective function and checking constraints for nlopt - h=1
+print (dim(df_comp %>% filter(eval_nlopt_1<=eval_bfs_1))[1]/dim(df_comp)[1])
+print (dim(df_comp %>% filter(eval_ineq_nlopt_1<=0.0001))[1]/dim(df_comp)[1])
+
+#Dynamic case
+
+df_comp <- data.frame()
+for (D in unique(nl_d_seq)){
+  res_bfs_0 <- as.numeric(res_dynamic[(res_dynamic$d1==D)&(res_dynamic$homogeneous==0)&(res_dynamic$variable=='i')
+                                    ,c('age3','age4','age5','age6','age7','age8','age9')])
+  eval_bfs_0 <- as.numeric(res_dynamic[(res_dynamic$d1==D)&(res_dynamic$homogeneous==0)&(res_dynamic$variable=='i')
+                                     ,'value'])
+  res_bfs_1 <- as.numeric(res_dynamic[(res_dynamic$d1==D)&(res_dynamic$homogeneous==1)&(res_dynamic$variable=='i')
+                                    ,c('age3','age4','age5','age6','age7','age8','age9')])
+  eval_bfs_1 <- as.numeric(res_dynamic[(res_dynamic$d1==D)&(res_dynamic$homogeneous==1)&(res_dynamic$variable=='i')
+                                     ,'value'])
+  
+  eval_nlopt_0 <- nlopt_d0[1,as.character(D)]
+  res_nlopt_0 <- nlopt_d0[2:8,as.character(D)]
+  eval_nlopt_1 <- nlopt_d1[1,as.character(D)]
+  res_nlopt_1 <- nlopt_d1[2:8,as.character(D)]
+  
+  df_comp <- rbind(df_comp, data.frame("eval_bfs_0"=eval_bfs_0,"eval_nlopt_0"=eval_nlopt_0,
+                                       "eval_bfs_1"=eval_bfs_1,"eval_nlopt_1"=eval_nlopt_1,"d1"=D))
+}
+#Comparing objective function - h=0
+print (dim(df_comp %>% filter(eval_nlopt_0<=eval_bfs_0))[1]/dim(df_comp)[1])
+#Comparing objective function - h=0
+print (dim(df_comp %>% filter(eval_nlopt_1<=eval_bfs_1))[1]/dim(df_comp)[1])
 
--- optimisation-epi/nlopt-ngroups.R
+++ optimisation-epi/nlopt-ngroups.R
@@ -11,9 +11,9 @@ q_seq <- c(0.1, 0.25, 0.5, 0.75, 1) #just try two quantities at first
 
 
 # Choose dimensionality of x here: -----
-n_x <- 4
+n_x <- 7
 unroll_x <- function(x, sub=1) #set sub to 0 for static model where no doses go to children
-  c(sub,sub,x[1],x[1],x[2],x[2],x[3],x[3],x[4])
+  c(sub,sub,x[1],x[2],x[3],x[4],x[5],x[6],x[7])
 
 
 # Solve for various Q's (static) or deltas (dynamic) ------
@@ -21,49 +21,54 @@ unroll_x <- function(x, sub=1) #set sub to 0 for static model where no doses go 
 opt_problem <- function(q_seq,h=FALSE, static = TRUE){
   
   sol <- sapply(q_seq, function(Q) {
-    
     # Optimisation problem: define objective function for a given Q, h:
     if(static)
-      eval_f <- function(x) model_fd_static(unroll_x(x), 
+      eval_f <- function(x) model_fd_static(unroll_x(x,sub=0), 
                                             homogen = h, 
-                                            outcome = outcome_nlopt)
+                                            outcome = outcome_nlopt,
+                                            ret = 1)
     else
       eval_f <- function(x) model_fd_dynamic(model = "pars_le_fast", 
                                              fd = unroll_x(x), 
                                              d1 = Q, 
                                              outcome = outcome_nlopt,
+                                             ret = 1,
                                              homogen = h)
     
     # Equality constraints
     if(static)
-      eval_g_eq <- function(x) c(sum(unroll_x(x)*pop)-Q)
+      #GROUP NUMBER - Need to change this constraint according to the number of groups
+      eval_g_ineq <- function(x) c(x[1]*pop[3]+x[2]*pop[4]+x[3]*pop[5]+x[4]*pop[6]+x[5]*pop[7]+x[6]*pop[8]+x[7]*pop[9]-Q,
+                                   -x[1]*pop[3]-x[2]*pop[4]-x[3]*pop[5]-x[4]*pop[6]-x[5]*pop[7]-x[6]*pop[8]-x[7]*pop[9]+Q)
     else
-      eval_g_eq <- NULL
+      eval_g_ineq <- NULL
     
     # Lower and upper bounds
-    lb <- rep(0,n_x)
     ub <- rep(1,n_x)
     
     # Initial values
-    if(static)
-      x0 <- rep(0, n_x)
-    else
-      x0 <- rep(1, n_x)
-    
+    if(static){
+      lb <- rep(0,n_x)
+      x0 <- rep(0.01, n_x)
+    } else {
+      lb <- rep(0.01,n_x)
+      x0 <- rep(0.01, n_x)
+    }
     # Set optimization options.
-    opts <- list( "algorithm"= "NLOPT_GN_ISRES",
+    opts <- list( "algorithm"= "NLOPT_LN_COBYLA",#"NLOPT_GN_ISRES"
                   "xtol_rel"= 1.0e-4,
-                  "maxeval"= 1e03,
-                  "local_opts" = list( "algorithm" = "NLOPT_LN_NELDERMEAD", 
-                                       "xtol_rel" = 1.0e-4 ),
+                  "maxeval"= 100,
+                  #"local_opts" = list( "algorithm" = "NLOPT_LN_NELDERMEAD",
+                  #                     "xtol_rel" = 1.0e-4 ),
                   "print_level" = 0 )
+    
     
     # Solving
     res <- nloptr ( x0 = x0,
                     eval_f = eval_f,
                     lb = lb,
                     ub = ub,
-                    eval_g_eq = eval_g_eq,
+                    eval_g_ineq = eval_g_ineq,
                     opts = opts
     )
     c(res$objective, res$solution)
@@ -73,12 +78,17 @@ opt_problem <- function(q_seq,h=FALSE, static = TRUE){
   sol
 }
 
-nl_q_seq <- rep(seq(0.1, 0.7, 0.1), each = 5)
-nl_d_seq <- rep(c(1000,400,200,100,50), each=5)
+nl_q_seq <- seq(0.1, 0.7, 0.1)#Variance analysis are unnecessary since the solution only changes with the initial values
+nl_d_seq <- c(1000,400,200,100,50)
+ptm <- proc.time()
 nlopt_s0 <- opt_problem(nl_q_seq, 0)
+proc.time() - ptm
 nlopt_s1 <- opt_problem(nl_q_seq, 1)
+ptm <- proc.time()
 nlopt_d0 <- opt_problem(nl_d_seq, 0, static = F)
+proc.time() - ptm
 nlopt_d1 <- opt_problem(nl_d_seq, 1, static = F)
 
-# save(nl_q_seq, nl_d_seq,
-     # nlopt_s0, nlopt_s1, nlopt_d0, nlopt_d1, file = "results/wip-nl-solutions4.Rdata")
+#save(nl_q_seq, nlopt_s0, file = "results/wip-nl-solutions5-s-h0-D.Rdata")
+save(nl_q_seq, nl_d_seq,
+     nlopt_s0, nlopt_s1, nlopt_d0, nlopt_d1, file = "results/wip-nl-solutions7-D.Rdata")
