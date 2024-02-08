a57f4c9c70acf8721a089d99dbace998ac612033
Leonardo Wang <yuyangwang42@uchicago.edu>
2024-02-08 10:53:28 -0600

    Pooling functions together.
    Still working on it

---
+++ review/initiate.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(ggpubr)
+library(latex2exp)
+library(shades)
+library(grid)
+theme_set(theme_minimal(base_size = 10))
+
+sounce("setup/parameters")
+sounce("setup/epi_models")
+
+
+
+
